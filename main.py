b091d6db8fe3bb8cffbec91e94efee107c6f384d
Kishor <contact@kishorramanan.com>
2024-08-12 01:11:18 +0530

    Init

---
+++ main.py
@@ -0,0 +1,164 @@
+from unsloth import FastLanguageModel
+import torch
+
+max_seq_length = 2048
+dtype = None
+load_in_4bit = False
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name="unsloth/Meta-Llama-3.1-8B",
+    max_seq_length=max_seq_length,
+    dtype=dtype,
+    load_in_4bit=load_in_4bit,
+    load_in_8bit_fp32_cpu_offload=True,
+    device_map={"module_name": "cpu"},
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r=16,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+    ],
+    lora_alpha=16,
+    lora_dropout=0,
+    bias="none",
+    use_gradient_checkpointing="unsloth",
+    random_state=3407,
+    use_rslora=False,
+    loftq_config=None,
+)
+
+prompt_template = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+EOS_TOKEN = tokenizer.eos_token
+
+
+def formatting_prompts_func(examples):
+    instructions = examples["instruction"]
+    inputs = examples["input"]
+    outputs = examples["response"]
+    texts = []
+    for instruction, input, output in zip(instructions, inputs, outputs):
+        text = prompt_template.format(instruction, input, output) + EOS_TOKEN
+        texts.append(text)
+    return {
+        "text": texts,
+    }
+
+
+from datasets import load_dataset
+
+dataset = load_dataset(
+    "json", data_files={"train": "/home/k1sh0r/AI/data.json"}, split="train"
+)
+dataset = dataset.map(
+    formatting_prompts_func,
+    batched=True,
+)
+
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from unsloth import is_bfloat16_supported
+
+trainer = SFTTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    train_dataset=dataset,
+    dataset_text_field="text",
+    max_seq_length=max_seq_length,
+    dataset_num_proc=2,
+    packing=False,
+    args=TrainingArguments(
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=4,
+        warmup_steps=5,
+        max_steps=126,
+        learning_rate=2e-4,
+        fp16=not is_bfloat16_supported(),
+        bf16=is_bfloat16_supported(),
+        logging_steps=1,
+        optim="adamw_8bit",
+        weight_decay=0.01,
+        lr_scheduler_type="linear",
+        seed=3407,
+        output_dir="outputs",
+    ),
+)
+
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
+
+trainer_stats = trainer.train()
+
+used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+used_percentage = round(used_memory / max_memory * 100, 3)
+lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
+print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+print(
+    f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training."
+)
+print(f"Peak reserved memory = {used_memory} GB.")
+print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+
+FastLanguageModel.for_inference(model)
+
+exit_flag = False
+while not exit_flag:
+    user_input = input("Press enter to give prompt or type exit:")
+    if user_input == "exit":
+        exit_flag = True
+        continue
+    inputs = tokenizer(
+        [
+            prompt_template.format(
+                input("Enter your instruction prompt: "),
+                input("Enter your input prompt or leave it blank: "),
+                "",
+            )
+        ],
+        return_tensors="pt",
+    ).to("cuda")
+
+    from transformers import TextStreamer
+
+    text_streamer = TextStreamer(tokenizer)
+    _ = model.generate(**inputs, streamer=text_streamer, max_new_tokens=128)
+
+print("Saving LoRA Adapters...")
+print("Saving LoRA Model Adapter...")
+model.save_pretrained("kishor_personal_lora_model")
+print("Saving LoRA Adapter Tokenizer...")
+tokenizer.save_pretrained("kishor_personal_lora_tokenizer")
+print("Successfully saved LoRA Adapters")
+
+print("Saving Model in 16bit (float16)...")
+model.save_pretrained_merged(
+    "kishor_personal_model_16bit", tokenizer, save_method="merged_16bit"
+)
+
+print("Saving model in q4_k_m GGUF format")
+model.save_pretrained_gguf(
+    "kishor_personal_model_gguf", tokenizer, quanitzation_method="q4_k_m"
+)
